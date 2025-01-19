8dc16d2f026dff726e9694f1493e524ede51d6ea
SheikhShahJahan <shahjahan.sheikh@edu.esiee.fr>
2025-01-19 21:58:15 +0100

    Create room.py

---
+++ room.py
@@ -0,0 +1,11 @@
+class Room: #création des objets/classes pièce, ss-pièce et objet
+    def __init__(self, name, description=''):
+        self.name = name
+        self.description = description
+        self.exits_room = {} # pour les pièces dans le couloir
+        self.exits_ssroom = {} # pour les sous-pièces dans les autres pièces
+        self.ssrooms = {}
+
+    def get_long_description_room(self):
+        return f"\n{self.description}\n"
+    
