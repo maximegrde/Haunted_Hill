611c3f81f37ee724a5d884b36f619fd204a51b0a
maxime ♥✿ <maxime.grandclaude@edu.esiee.fr>
2025-01-19 15:58:24 +0100

    Haunted_Hill.py
    pylint opti

--- Haunted_Hill.py
+++ Haunted_Hill.py
@@ -1,172 +1,88 @@
-# Description: Game class
-
-# Import modules
-
 from floor import Floor
 from room import Room
 from player import Player
 from command import Command
 from actions import Actions
 
-class Game:
 
-    # Constructor
+class Game:
+    """Classe principale du jeu qui gère l'initialisation et le déroulement."""
+
     def __init__(self):
+        """Constructeur de la classe Game."""
         self.finished = False
         self.floors = {}
-        self.rooms = {}
-        self.srooms = {}
-        # self.floors = {"Sous-sol":{"Pièce 1":{"générateur":{câble, bouton de démarrage}}}, "étage 1" : {"Pièce 1":{"générateur":{câble, bouton de démarrage}}}, etc }
         self.commands = {}
         self.player = None
-    
-    # Setup the game
+
     def setup(self):
+        """Initialise les éléments du jeu."""
 
-        # Setup commands
+        # Initialisation des commandes
+        self.commands = {
+            "help": Command("help", " : afficher cette aide", Actions.help, 0),
+            "quit": Command("quit", " : quitter le jeu", Actions.quit, 0),
+            "go": Command("go", " <direction> : se déplacer dans une direction", Actions.go, 1),
+        }
 
-        help = Command("help", " : afficher cette aide", Actions.help, 0)
-        self.commands["help"] = help
-        quit = Command("quit", " : quitter le jeu", Actions.quit, 0)
-        self.commands["quit"] = quit
-        go = Command("go", " <direction> : se déplacer dans une direction cardinale (N, E, S, O)", Actions.go, 1)
-        self.commands["go"] = go
-        
-        """ 
-        Les setups floors/rooms/ss-rooms décrivent les ambiances !
-        """
-        # Setup floors
-
+        # Création des étages
         sous_sol = Floor("Sous-sol", "Vous arrivez dans le sous-sol du manoir.")
-        salle_cachee = Room("Salle cachée", "Vous traversez le mur que vous venez de détruire et vous arrivez dans une pièce sombre.")
-        tiroir = SRoom("Tiroir","Il s'agit d'un tiroir que vous pouvez ouvrir.")
-        floors[self.floors(sous_sol)]=self.rooms #ajout du sous-sol dans le dict floors 
-        floors[0][0]=self.salle_cachee #ajout de la salle cachée au sous-sol
-        floors[0][0][0]=self.tiroir #ajout du tiroir dans la salle cachée
-
-        floors[0][1]=self.cachee
-        floors[0][2]=self.monstres
-        floors[0][3]=self.generateur
-        floors[0][4]=self.fissures
-        floors[0][5]=self.cle
-        floors[0][6]=self.souvenir
-        floors[0][7]=self.essence
-
-        rez_de_chaussee = Floor("Rez-de-chaussée", "Vous arrivez dans le rez-de-chaussée.")
-        floors+=self.floors(rez_de_chaussée)
+        rez_de_chaussee = Floor("Rez-de-chaussée", "Vous arrivez au rez-de-chaussée.")
         premier_etage = Floor("Premier étage", "Vous arrivez au premier étage.")
-        floors+=self.floors(premier_etage)
-        deuxieme_etage = Floor("Deuxième étage", "Vous arrivez au deuxième étage.")
-        floors+=self.floors(deuxieme_etage)
         toit = Floor("Toit", "Vous arrivez sur le toit.")
-        floors+=self.floors(toit)
-        
-        # Setup rooms
 
-        # Couloirs pour chaque étage (créer un fichier contenant les couloirs de chaque étage)
-        couloirsol = Room("Forest", "dans une forêt enchantée. Vous entendez une brise légère à travers la cime des arbres.")
-        self.rooms.append(forest)
-        couloirrdc = Room("Tower", "dans une immense tour en pierre qui s'élève au dessus des nuages.")
-        self.rooms.append(tower)
-        couloirpremier = Room("Cave", "dans une grotte profonde et sombre. Des voix semblent provenir des profondeurs.")
-        self.rooms.append(cave)
-        couloirdeuxieme = Room("Cottage", "dans un petit chalet pittoresque avec un toit de chaume. Une épaisse fumée verte sort de la cheminée.")
-        self.rooms.append(cottage)
-        couloirtoit = Room("Swamp", "dans un marécage sombre et ténébreux. L'eau bouillonne, les abords sont vaseux.")
-        self.rooms.append(swamp)
+        self.floors = {
+            "sous_sol": sous_sol,
+            "rez_de_chaussee": rez_de_chaussee,
+            "premier_etage": premier_etage,
+            "toit": toit,
+        }
 
-        couloirsol.exits = {"N" : cave, "E" : tower, "S" : castle, "O" : None}
-        couloirrdc.exits = {"N" : None, "E" : None, "S" : tower, "O" : cave}
-        couloirpremier.exits = {"N" : cottage, "E" : None, "S" : swamp, "O" : forest}
-        couloirdeuxieme.exits = {"N" : None, "E" : cottage, "S" : forest, "O" : None}
-        couloirtoit.exits = {"N" : tower, "E" : None, "S" : None, "O" : castle}
+        # Exemple de création de salles et configuration des sorties
+        forest = Room("Forêt", "Une forêt enchantée avec une brise légère.")
+        tower = Room("Tour", "Une tour immense qui s'élève au-dessus des nuages.")
 
-        # Rooms du floor 0
-        # Rooms du floor 1
-        # Rooms du floor 2
-        # Rooms du floor toit
+        forest.exits = {"N": tower, "E": None, "S": None, "O": None}
+        tower.exits = {"N": None, "E": None, "S": forest, "O": None}
 
+        sous_sol.rooms = {
+            "forest": forest,
+            "tower": tower,
+        }
 
-        # Setup sous-rooms (endroits à fouiller dans chaque pièce)
-        
+        # Initialisation du joueur
+        self.player = Player(input("\nEntrez votre nom : "))
+        self.player.current_room = forest
 
-
-
-
-
-
-
-        # Create exits for floors, rooms, srooms
-
-        # Exits for floors
-
-        sous_sol.exits = {"N" : cave, "E" : tower, "S" : castle, "O" : None}
-        rez_de_chaussee.exits = {"N" : None, "E" : None, "S" : tower, "O" : cave}
-        premier_etage.exits = {"N" : cottage, "E" : None, "S" : swamp, "O" : forest}
-        deuxieme_etage.exits = {"N" : None, "E" : cottage, "S" : forest, "O" : None}
-        toit.exits = {"N" : tower, "E" : None, "S" : None, "O" : castle}
-
-        # Exits for rooms
-
-        forest.exits = {"N" : cave, "E" : tower, "S" : castle, "O" : None}
-        tower.exits = {"N" : cottage, "E" : None, "S" : swamp, "O" : forest}
-        cave.exits = {"N" : None, "E" : cottage, "S" : forest, "O" : None}
-        cottage.exits = {"N" : None, "E" : None, "S" : tower, "O" : cave}
-        swamp.exits = {"N" : tower, "E" : None, "S" : None, "O" : castle}
-        castle.exits = {"N" : forest, "E" : swamp, "S" : None, "O" : None}
-
-        # Exits for srooms
-
-        forest.exits = {"N" : cave, "E" : tower, "S" : castle, "O" : None}
-        tower.exits = {"N" : cottage, "E" : None, "S" : swamp, "O" : forest}
-        cave.exits = {"N" : None, "E" : cottage, "S" : forest, "O" : None}
-        cottage.exits = {"N" : None, "E" : None, "S" : tower, "O" : cave}
-        swamp.exits = {"N" : tower, "E" : None, "S" : None, "O" : castle}
-        castle.exits = {"N" : forest, "E" : swamp, "S" : None, "O" : None}
-
-        # Setup player and starting room
-
-        self.player = Player(input("\nEntrez votre nom: "))
-        self.player.current_room = swamp
-
-    # Play the game
     def play(self):
+        """Lance le jeu."""
         self.setup()
         self.print_welcome()
-        # Loop until the game is finished
         while not self.finished:
-            # Get the command from the player
             self.process_command(input("> "))
-        return None
 
-    # Process the command entered by the player
-    def process_command(self, command_string) -> None:
-
-        # Split the command string into a list of words
+    def process_command(self, command_string):
+        """Traite la commande saisie par le joueur."""
         list_of_words = command_string.split(" ")
-
         command_word = list_of_words[0]
 
-        # If the command is not recognized, print an error message
-        if command_word not in self.commands.keys():
-            print(f"\nCommande '{command_word}' non reconnue. Entrez 'help' pour voir la liste des commandes disponibles.\n")
-        # If the command is recognized, execute it
+        if command_word not in self.commands:
+            print(f"\nCommande '{command_word}' non reconnue. Entrez 'help' pour voir les commandes disponibles.\n")
         else:
             command = self.commands[command_word]
             command.action(self, list_of_words, command.number_of_parameters)
 
-    # Print the welcome message
     def print_welcome(self):
+        """Affiche le message de bienvenue."""
         print(f"\nBienvenue {self.player.name} dans ce jeu d'aventure !")
         print("Entrez 'help' si vous avez besoin d'aide.")
-        #
         print(self.player.current_room.get_long_description())
-    
+
 
 def main():
-    # Create a game object and play the game
+    """Point d'entrée principal du jeu."""
     Game().play()
-    
+
 
 if __name__ == "__main__":
     main()
